407712fc0fbcf1f967a38ae53c9e71d5ffe177c7
maximovavs <200645585+maximovavs@users.noreply.github.com>
2025-09-02 17:26:45 +0300

    Delete test_radiation.py

--- test_radiation.py
+++
@@ -1,5 +0,0 @@
-from radiation import get_radiation     # импорт из твоего же репо
-
-# координаты — любые, лишь бы рядом была станция
-print("Калининград:", get_radiation(54.71, 20.45))
-print("Лимассол:   ", get_radiation(34.70, 33.02))
